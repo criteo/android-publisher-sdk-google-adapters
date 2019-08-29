54bb24e90c4547c2b06f57792989e163952fbad6
Divya Kali Ranga Fnu <dkr.fnu@criteo.com>
2019-08-29 12:07:55 -0700

    Add logic to identify username in the build script
    Change-Id: I812413d931738062dbd2a8131c53158e1448b867
    JIRA: EE-463

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,17 +6,20 @@ cd build/output
 
 PUBLISHER_SDK_CONFIGURATION="release"
 USERNAME=$(whoami)
+if [ $USERNAME = "jenkins" ]; then
+	USERNAME="qabot"
+fi
 
 echo "Downloading $PUBLISHER_SDK_CONFIGURATION build for publisher-sdk as $USERNAME from mochi... \n"
 rm -rf mochi
 
-git clone ssh://qabot@review.criteois.lan:29418/pub-sdk/mochi
+git clone ssh://$USERNAME@review.criteois.lan:29418/pub-sdk/mochi
 
 echo "Clone complete"
 
 cd mochi
 echo "Using $ANDROID_HOME as android sdk"
-#echo "sdk.dir=/Users/qabot/Library/Android/sdk" >> local.properties
+#echo "sdk.dir=/Users/$USERNAME/Library/Android/sdk" >> local.properties
 
 ./gradlew clean build
 cp -f publisher-sdk/build/outputs/aar/publisher-sdk-$PUBLISHER_SDK_CONFIGURATION.aar ../../../publisher-sdk-release/
