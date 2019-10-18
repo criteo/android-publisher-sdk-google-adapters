43ddb4ec93d928e9a19c4cbb51e3bbfd5c2e8484
Sneha Pathrose <s.pathrose@criteo.com>
2019-10-18 10:38:50 -0700

    Modifying azure scripts to rename com.google.ads.mediation to com.criteo.mediation.google
    Change-Id: I2347a31b6e4ba056c85ae23e2212065463412628

--- scripts/azureDeploy.sh
+++ scripts/azureDeploy.sh
@@ -1,20 +1,20 @@
 echo "Creating local maven repository..."
 #replace release with the expected version
 export release=$1
-mvn install:install-file -Dfile=mediation-release.aar -DgroupId=com.google.ads.mediation -DartifactId=criteo -Dversion=$release -Dpackaging=aar -DpomFile=criteo-${release}.pom -DlocalRepositoryPath=android/
+mvn install:install-file -Dfile=mediation-release.aar -DgroupId=com.criteo.mediation.google -DartifactId=criteo -Dversion=$release -Dpackaging=aar -DpomFile=criteo-adapter-${release}.pom -DlocalRepositoryPath=android/
 
 export AZURE_STORAGE_ACCOUNT=pubsdkuseprod
 export AZURE_STORAGE_KEY=IBXkbamPEDzFFvLFgjL8bG5v7GOLy/2HY2xMVtgXICxSXG/AYYP57Xme9lxNgcoaznc2XGdye/zDT7fPUYrXbA==
 
 export container_name=publishersdk
-export aar_blob_name=android/com/google/ads/mediation/criteo/$release/criteo-${release}.aar
-export aar_file_to_upload=android/com/google/ads/mediation/criteo/$release/criteo-${release}.aar
+export aar_blob_name=android/com/criteo/mediation/google/criteo-adapter/$release/criteo-adapter-${release}.aar
+export aar_file_to_upload=android/com/criteo/mediation/google/criteo-adapter/$release/criteo-adapter-${release}.aar
 
-export pom_blob_name=android/com/google/ads/mediation/criteo/$release/criteo-${release}.pom
-export pom_file_to_upload=android/com/google/ads/mediation/criteo/$release/criteo-${release}.pom
+export pom_blob_name=android/com/criteo/mediation/google/criteo-adapter/$release/criteo-adapter-${release}.pom
+export pom_file_to_upload=android/com/criteo/mediation/google/criteo-adapter/$release/criteo-adapter-${release}.pom
 
-export mvn_blob_name=android/com/google/ads/mediation/criteo/maven-metadata-local.xml
-export mvn_file_to_upload=android/com/google/ads/mediation/criteo/maven-metadata-local.xml
+export mvn_blob_name=android/com/criteo/mediation/google/criteo-adapter/maven-metadata-local.xml
+export mvn_file_to_upload=android/com/criteo/mediation/google/criteo-adapter/maven-metadata-local.xml
 
 echo "Uploading the aar file..."
 az storage blob upload --container-name $container_name --file $aar_file_to_upload --name $aar_blob_name
