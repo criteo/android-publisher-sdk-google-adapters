e48ddfde27a8f15ff0c2a8315c4e6c5397673b19
p.turpin <p.turpin@criteo.com>
2020-04-22 13:32:53 +0200

    Publish the adapter for every merged commits
    This reproduce the behavior of the SDK. All successful versions are
    published to the Nexus Prod and may be used as RC. Hence we do not need
    to manually upload the artifact, nor to provide AAR files manually.
    
    Change-Id: Ie3a8f40eb2f06889c27bd5a1f2241c781b9f6439
    JIRA: EE-1014

--- scripts/build-merged.sh
+++ scripts/build-merged.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -l
 
-echo "Update this file to run a clean + build on android-google-mediation for buld+test+publish to nexus"
-#ORIGINAL_VERSION=2.2.1
-#VERSION=$ORIGINAL_VERSION-criteo-$(date -u +%Y%m%d%H%M%S)
-sh scripts/build-pre-submit.sh
-#sh gradlew publish -Pversion=$VERSION -PpublishUrl="http://nexus.criteo.prod/content/repositories/criteo.android.releases/"
+# Run this script to assemble and publish artifacts to Criteo nexus.
+
+set -Eeuo pipefail
+
+echo "Building and publishing artifacts to nexus"
+
+./scripts/build-pre-submit.sh
+
+# The Nexus prod is acting as a preprod
+./gradlew publishReleasePublicationToNexusProdRepository -PappendTimestamp=true
