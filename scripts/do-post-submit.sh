42bc8761d19166461addae92a09409de6db64271
p.turpin <p.turpin@criteo.com>
2020-07-01 18:48:00 +0200

    Replace internal Nexus by public Bintray
    Change-Id: I27ebdeb525f2825fea18c1089fd69d9ef9e264ba
    JIRA: EE-1138

--- scripts/do-post-submit.sh
+++ scripts/do-post-submit.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -l
 
-# Run this script to assemble and publish artifacts to Criteo nexus.
+# Run this script to assemble and publish development artifacts.
 # If you run this script manually, make sure to have the Android tools available. Else, you may
 # instead run the ./scripts/build-merged.sh script, to wrap the build in a docker container with
 # sufficient tooling.
 
 set -Eeuo pipefail
 
-echo "Building and publishing artifacts to nexus"
+echo "Building and publishing development artifacts"
 
 ./scripts/do-pre-submit.sh
 
-# The Nexus prod is acting as a preprod
-./gradlew publishReleasePublicationToNexusProdRepository
+./gradlew bintrayUpload
