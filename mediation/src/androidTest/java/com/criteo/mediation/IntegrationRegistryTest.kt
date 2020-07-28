6cac47b9d17862e64f7c28a776aec093b8a0cee4
Pierre Turpin <p.turpin@criteo.com>
2020-07-28 10:59:29 +0200

    Test that profile ID of mediation adapter is detected (#19)
    Test that profile ID of mediation adapter is detected
    
    The SDK automatically find the mediation adapter by looking for its
    classes in the runtime classpath. This test verify that the SDK do a
    good job.

---
+++ mediation/src/androidTest/java/com/criteo/mediation/IntegrationRegistryTest.kt
@@ -0,0 +1,27 @@
+package com.criteo.mediation
+
+import com.criteo.publisher.integration.Integration
+import com.criteo.publisher.integration.IntegrationRegistry
+import com.criteo.publisher.mock.MockedDependenciesRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+class IntegrationRegistryTest {
+
+  @Rule
+  @JvmField
+  val mockedDependenciesRule = MockedDependenciesRule()
+
+  @Inject
+  private lateinit var integrationRegistry: IntegrationRegistry
+
+  @Test
+  fun profileId_WithThisAdapterInClasspath_DetectItAutomatically() {
+    val profileId = integrationRegistry.profileId
+
+    assertThat(profileId).isEqualTo(Integration.ADMOB_MEDIATION.profileId)
+  }
+
+}
